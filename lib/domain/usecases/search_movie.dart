b64581293d5d0b49153205406140af7e2d13c305
Sibongiseni Ndebele <43204005+Sbongslany@users.noreply.github.com>
2024-04-17 16:05:53 +0200

    flutter test SEARCH MOVIE

---
+++ lib/domain/usecases/search_movie.dart
@@ -0,0 +1,13 @@
+import '../entities/Movie.dart';
+import '../repositories/movie_repository.dart';
+
+class SearchMovies{
+  final MovieRepository repository;
+
+  SearchMovies(this.repository);
+
+  Future<List<Movie>>call(String query)async{
+    return await repository.searchMovies(query);
+  }
+
+}
---
+++ test/domain/usecases/search_movies_test.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:movie_app/domain/repositories/movie_repository.dart';
+import 'package:mockito/mockito.dart';
+import 'package:movie_app/domain/usecases/search_movie.dart';
+import 'package:movie_app/domain/entities/Movie.dart';
+import 'get_trending_movies_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<MovieRepository>()])
+
+void main(){
+
+  late SearchMovies usecase;
+  late MockMovieRepository mockMovieRepository;
+
+  setUp(()  {
+  mockMovieRepository =  MockMovieRepository();
+  usecase = SearchMovies(mockMovieRepository);
+  });
+
+  final tQuery = 'Inception';
+
+  final tMoviesList = [
+    Movie(id: 1, title: 'Test Movie 1', overview: 'Disc 1', posterPath: '/image1'),
+    Movie(id: 2, title: 'Test Movie 2', overview: 'Disc 3', posterPath: '/image2'),
+    Movie(id: 3, title: 'Test Movie 3', overview: 'Disc 3', posterPath: '/image3')
+
+  ];
+
+  test('should get movies from the query fro the repository', () async {
+
+    //arrange
+    when(mockMovieRepository.searchMovies(any))
+        .thenAnswer((_) async => tMoviesList);
+    //act
+    final result = await usecase(tQuery);
+
+    //assert
+    expect(result, tMoviesList);
+    verify(mockMovieRepository.searchMovies(tQuery));
+    verifyNoMoreInteractions(mockMovieRepository);
+
+  });
+
+}
